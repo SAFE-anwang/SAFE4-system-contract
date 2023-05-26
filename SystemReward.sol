77b9467518dcbb5afb7b046fb3dbb1509369d249
lemengbin <lemengbin@163.com>
2023-05-26 15:43:47 +0800

    check reward amount

--- SystemReward.sol
+++ SystemReward.sol
@@ -15,6 +15,7 @@ contract SystemReward is ISystemReward, System {
         require(isMN(_mnAddr), "invalid masternode");
         require(_snAmount > 0, "invalid supernode reward");
         require(_mnAmount > 0, "invalid masternode reward");
+        require(_snAmount + _mnAmount == msg.value, "invalid amount");
         ISuperNode sn = ISuperNode(SUPERNODE_PROXY_ADDR);
         sn.reward{value: _snAmount}(_snAddr);
         IMasterNode mn = IMasterNode(MASTERNODE_PROXY_ADDR);
