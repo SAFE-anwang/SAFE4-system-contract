7c6e173fd6d294dbd792bae0ae2f1a2983947909
lemengbin <lemengbin@163.com>
2023-07-10 22:11:57 +0800

    add MasterNodeState and SuperNodeState

--- interfaces/INodeState.sol
+++ interfaces/INodeState.sol
@@ -2,5 +2,12 @@
 pragma solidity ^0.8.0;
 
 interface INodeState {
+    struct StateEntry {
+        address caller;
+        uint8 state;
+    }
+
     function uploadState(uint[] memory _ids, uint8[] memory _states) external;
+    function getAllState() external view returns (uint[] memory, uint8[] memory);
+    function getEntries(uint _id) external view returns (StateEntry[] memory);
 }
