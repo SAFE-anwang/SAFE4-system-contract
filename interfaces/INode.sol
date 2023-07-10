65ec3e028fc8b3339835477a89f2974fc8d81f10
lemengbin <lemengbin@163.com>
2023-07-10 21:29:48 +0800

    add state struct

--- interfaces/INode.sol
+++ interfaces/INode.sol
@@ -15,16 +15,24 @@ interface INode {
         uint voter; // voter percent [40%, 50%]
     }
 
+    struct StateInfo {
+        uint8 state;
+        uint height;
+    }
+
     event SystemReward(address _nodeAddr, uint _nodeType, address _addr, uint _rewardType, uint _amount);
 
     function appendRegister(address _addr, uint _lockDay) external payable;
     function turnRegister(address _addr, uint _lockID) external;
     function reward(address _addr) external payable;
-    function getNum() external view returns (uint);
 
     function changeAddress(address _addr, address _newAddr) external;
     function changeEnode(address _addr, string memory _newEnode) external;
     function changeDescription(address _addr, string memory _newDescription) external;
+    function changeOfficial(address _addr) external;
+    function changeState(uint _id, uint8 _state) external;
+
+    function getNum() external view returns (uint);
 
     function exist(address _addr) external view returns (bool);
     function existID(uint _id) external view returns (bool);
