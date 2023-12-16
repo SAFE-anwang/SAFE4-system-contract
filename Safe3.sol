0f27907245ac10ce4dc49cd8ff1a25b6c78e52b1
lemengbin <lemengbin@163.com>
2023-12-16 19:52:08 +0800

    fix Safe3 bug

--- Safe3.sol
+++ Safe3.sol
@@ -7,20 +7,20 @@ import "./utils/Base58.sol";
 contract Safe3 is ISafe3, System {
     // available safe3
     uint num;
-    bytes20[] keyIDs;
-    mapping(bytes20 => Safe3Info) availables;
+    bytes[] keyIDs;
+    mapping(bytes => Safe3Info) availables;
 
     // locked safe3
     uint lockedNum;
-    bytes20[] lockedKeyIDs;
-    mapping(bytes20 => Safe3LockInfo[]) locks;
+    bytes[] lockedKeyIDs;
+    mapping(bytes => Safe3LockInfo[]) locks;
 
     function redeemAvailable(bytes memory _pubkey, bytes memory _sig) public override {
-        bytes20 keyID = getKeyIDFromPubkey(_pubkey);
+        bytes memory keyID = getKeyIDFromPubkey(_pubkey);
         require(availables[keyID].amount > 0, "non-existent available amount");
         require(availables[keyID].redeemHeight == 0, "has redeemed");
 
-        bytes32 h = sha256(abi.encodePacked(getSafe3Addr(_pubkey)));
+        bytes32 h = sha256(abi.encodePacked(string(Base58.encode(keyID))));
         bytes32 msgHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", h));
         require(verifySig(_pubkey, msgHash, _sig), "invalid signature");
 
@@ -31,7 +31,7 @@ contract Safe3 is ISafe3, System {
     }
 
     function redeemLocked(bytes memory _pubkey, bytes memory _sig) public override {
-        bytes20 keyID = getKeyIDFromPubkey(_pubkey);
+        bytes memory keyID = getKeyIDFromPubkey(_pubkey);
         for(uint i = 0; i < locks[keyID].length; i++) {
             Safe3LockInfo memory info = locks[keyID][i];
             if(info.amount == 0 || info.redeemHeight != 0) {
@@ -74,12 +74,12 @@ contract Safe3 is ISafe3, System {
     }
 
     function getAvailable(string memory _safe3Addr) public view override returns (Safe3Info memory) {
-        bytes20 keyID = bytesToBytes20(getKeyIDFromAddress(_safe3Addr), 0);
+        bytes memory keyID = getKeyIDFromAddress(_safe3Addr);
         return availables[keyID];
     }
 
     function getLocked(string memory _safe3Addr) public view override returns (Safe3LockInfo[] memory) {
-        bytes20 keyID = bytesToBytes20(getKeyIDFromAddress(_safe3Addr), 0);
+        bytes memory keyID = getKeyIDFromAddress(_safe3Addr);
         return locks[keyID];
     }
 
@@ -96,52 +96,43 @@ contract Safe3 is ISafe3, System {
         uint pos = 0;
         for(uint i = 0; i < lockedKeyIDs.length; i++) {
             Safe3LockInfo[] memory infos = locks[lockedKeyIDs[i]];
-            for(uint k = 0; k > infos.length; k++) {
+            for(uint k = 0; k < infos.length; k++) {
                 ret[pos++] = infos[k];
             }
         }
         return ret;
     }
 
-    function getKeyIDFromPubkey(bytes memory _pubkey) internal pure returns (bytes20) {
-        return ripemd160(abi.encodePacked(sha256(_pubkey)));
-    }
-
-    function getKeyIDFromAddress(string memory _safe3Addr) internal pure returns (bytes memory) {
-        bytes memory b =  Base58.decodeFromString(_safe3Addr);
-        bytes memory keyID = new bytes(20);
-        for(uint i = 0; i < 20; i++) {
-            keyID[i] = b[i + 1];
-        }
-        return keyID;
-    }
-
-    function getSafe3Addr(bytes memory _pubkey) internal pure returns (string memory) {
+    function getKeyIDFromPubkey(bytes memory _pubkey) internal pure returns (bytes memory) {
         bytes32 h = sha256(_pubkey);
-        bytes20 keyID = ripemd160(abi.encodePacked(h));
+        bytes20 r = ripemd160(abi.encodePacked(h));
         bytes memory t = new bytes(21);
         t[0] = 0x4c;
         for(uint i = 0; i < 20; i++) {
-            t[i + 1] = keyID[i];
+            t[i + 1] = r[i];
         }
-        bytes32 h1 = sha256(t);
-        bytes32 h2 = sha256(abi.encodePacked(h1));
+        h = sha256(t);
+        h = sha256(abi.encodePacked(h));
         bytes memory t2 = new bytes(25);
         for(uint i = 0; i < 21; i++) {
             t2[i] = t[i];
         }
         for(uint i = 0; i < 4; i++) {
-            t2[i + 21] = h2[i];
+            t2[i + 21] = h[i];
         }
-        string memory addr = string(Base58.encode(t2));
-        return addr;
+        return t2;
     }
 
-    function getSafe4Addr(bytes memory _pubkey) public pure returns (address addr) {
-        bytes32 b = keccak256(_pubkey);
-        assembly {
-            addr := mload(add(b, 12))
-        }
+    function getKeyIDFromAddress(string memory _safe3Addr) internal pure returns (bytes memory) {
+        return Base58.decodeFromString(_safe3Addr);
+    }
+
+    //function getSafe3Addr(bytes memory _pubkey) internal pure returns (string memory) {
+    //    return string(Base58.encode(getKeyIDFromPubkey(_pubkey)));
+    //}
+
+    function getSafe4Addr(bytes memory _pubkey) internal pure returns (address addr) {
+        return address(uint160(uint256(keccak256(_pubkey))));
     }
 
     function verifySig(bytes memory _pubkey, bytes32 _msgHash, bytes memory _sig) internal pure returns (bool) {
@@ -154,13 +145,5 @@ contract Safe3 is ISafe3, System {
             v := byte(0,mload(add(_sig ,96)))
         }
         return getSafe4Addr(_pubkey) == ecrecover(_msgHash, v, r, s);
-    }
-
-    function bytesToBytes20(bytes memory b, uint offset) internal pure returns (bytes20) {
-        bytes20 out;
-        for (uint i = 0; i < 20; i++) {
-            out |= bytes20(b[offset + i] & 0xFF) >> (i * 8);
-        }
-        return out;
     }
 }
